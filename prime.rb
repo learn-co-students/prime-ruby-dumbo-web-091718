f14fe8dcfa3cb4662f179e12d74c0640e6e886bd
Brian DiRito <bdirito@gmail.com>
2018-08-02 18:24:17 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  elsif integer == 2 || integer == 3
+    return true
+  elsif integer % 2 == 0 || integer % 3 == 0
+    return false
+  elsif integer % 5 == 0
+    return false
+  elsif integer > 6
+    loop do
+      if (integer - 1) % 6 == 0
+        return false
+      elsif Math.sqrt(integer + 1) % 6 == 0
+        return false
+
+      elsif (integer + 1) % 6 == 0
+        return true
+      else
+        return false
+        break
+      end
+    end
+  end
+end
