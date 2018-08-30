ac495521251b2f60ddff89ec0f279dbf8096372c
github username <github email address>
2018-08-29 22:35:56 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+#I'm looking for a number that will NOT have a remainder when divided.
+#prime numbers have to be divided by 1 and only one other natural number.
+# a number above 1 and between the numbers itself.
+# if there is a remainder on any
+require 'benchmark'
+
+puts Benchmark.measure {
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  (2..number/2).none?{|i| number % i == 0}
+
+
+
+
+end
+puts  prime?(11)
+}
