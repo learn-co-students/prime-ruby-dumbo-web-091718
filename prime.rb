716bc67be0ba71a05ed60acfa01175694dcd879e
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-07 17:01:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  return false if num == 1 || num == 0 || num == -1
+  i = 1
+  while i < num
+  
+   return false if num % i == 0 && i != 1
+   i += 1
+  
+  end
+ 
+ return true
+
+end
